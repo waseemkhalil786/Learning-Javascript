2b141f792fce4b566325e79018197b3ebed7a2c0
Waseem khalil <waseemkhalilworl@gmail.com>
2024-05-17 00:43:23 -0700

    learnig objects

---
+++ objcts/app.js
@@ -0,0 +1,26 @@
+//objects
+
+//This is array []
+
+// let my =["Name","Age","Study","Institute"]
+
+// console.log(my[""]);
+
+//start objects{}
+
+// let a = {
+//     fullname:"waseem khalil",
+//     age : "15",
+//     email : "wkdeveloper@gmail.com",
+//     study : "Intermediate",
+//     arry: ["0","uk","sk","wk"],
+//     fucar: function(){
+//         return www.wk.developer
+//     }
+// };
+// console.log(a.fullname);
+// console.log(a.age);
+// console.log(a.email);
+// console.log(a.study);
+// console.log(a.arry[3]);
+// console.log(a.fucar());
